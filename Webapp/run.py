2612f5f53842ab676326f67b0aa082ec1095c379
Agulaa <agnieszkaxrusin@gmail.com>
2018-05-12 13:46:01 +0200

    test

--- Webapp/run.py
+++ Webapp/run.py
@@ -1,10 +1,20 @@
-from app import create_app
+# from app import create_app
+#
+#
+# app = create_app()
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0')
 
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return 'Hello'
+    return 'hello :) '
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run()
+    
